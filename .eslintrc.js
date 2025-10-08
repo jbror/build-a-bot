d7844d5563fd2b61b4078485451750c959543241
jbror <reachingbror@gmail.com>
2025-10-08 15:58:35 +0200

    Adding configs for eslint and minor changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,11 @@ module.exports = {
     parser: '@babel/eslint-parser',
   },
   rules: {
+    indent: ['error', 2],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': 0,
     'operator-linebreak': 0,
+    'no-multiple-empty-lines': ['error', { max: 6, maxEOF: 2 }],
   },
 };
