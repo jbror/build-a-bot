4bd3701a303d199ae46a9fdcc8c9920a51e1b65b
jbror <reachingbror@gmail.com>
2025-10-09 11:06:52 +0200

    Refactor navigation styles for improved layout and consistency

--- src/App.vue
+++ src/App.vue
@@ -52,8 +52,23 @@ margin: 0 auto;
 
 ul{
  padding: 3px;
+display: flex;
+}
+
+.nav-item {
+display: inline-block;
+padding: 5px 10px;
+font-size: 22px;
+border-right: 1px solid #bbb;
+}
+
+.logo {
+vertical-align: middle;
+height: 30px;
 
 }
+
+
 
 
 
