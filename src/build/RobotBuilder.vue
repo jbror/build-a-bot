3875728cdb4d511488c8f4828a9649afbcc82709
jbror <reachingbror@gmail.com>
2025-10-13 22:10:33 +0200

    Format robot cost display using currency formatter in RobotBuilder component

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -49,7 +49,7 @@
     <tbody>
       <tr v-for="(robot, index) in cart" :key="index">
       <td>{{ robot.head.title }} </td>
-      <td class="cost">{{ robot.cost }} </td>
+      <td class="cost">{{ toCurrency(robot.cost) }} </td>
       </tr>
     </tbody>
   </table>
@@ -58,6 +58,7 @@
 
 <script>
 import parts from '../data/parts';
+import { toCurrency } from '../shared/formatters';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -105,6 +106,8 @@ export default {
       this.cart.push({ ...robot, cost });
       console.log(this.cart.length);
     },
+
+    toCurrency,
     selectNextHead() {
       this.selectedHeadIndex = getNextValidIndex(
         this.selectedHeadIndex,
