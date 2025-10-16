5ad279768a7d467468ec7f9f88acb14d41b4d661
jbror <reachingbror@gmail.com>
2025-10-16 09:58:54 +0200

    Refactor PartSelector to emit selected part on update and clean up part selection logic in RobotBuilder with improved layout for preview

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup>
-import { computed, ref } from 'vue';
+import { computed, ref, onUpdated } from 'vue';
 
 
 const props = defineProps({
@@ -22,6 +22,11 @@ const props = defineProps({
 const emit = defineEmits(['partSelected']);
 const selectedPartIndex = ref(0);
 const selectedPart = computed(() => props.parts[selectedPartIndex.value]);
+
+emit('partSelected', selectedPart);
+
+onUpdated(() => emit('partSelected', selectedPart));
+
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -38,7 +43,6 @@ const selectNextPart = () => {
     selectedPartIndex.value,
     props.parts.length,
   );
-  emit('partSelected', selectedPart);
 };
 
 const selectPreviousPart = () => {
@@ -46,7 +50,6 @@ const selectPreviousPart = () => {
     selectedPartIndex.value,
     props.parts.length,
   );
-  emit('partSelected', selectedPart);
 };
 </script>
 
--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()"> Add to Cart</button>
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.imageUrl" alt="head" />
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.imageUrl" class="rotate-left" alt="leftArm" />
+          <img :src="selectedRobot.torso.imageUrl" alt="torso" />
+          <img :src="selectedRobot.rightArm.imageUrl" class="rotate-right" alt=rightArm />
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.imageUrl" alt="base" />
+        </div>
+      </div>
+      <button class="add-to-cart" @click="addToCart()"> Add to Cart</button>
+    </div>
     <div class="top-row">
       <div class="robot-name">
         {{ selectedRobot.head.title }}
@@ -208,10 +223,39 @@ const addToCart = () => {
   position: relative;
 }
 
+
+.preview {
+  position: absolute;
+  top: -20px;
+  right: 0;
+  width: 310px;
+  height: 310px;
+  padding: 5px;
+}
+
+.preview-content {
+  border: 1px solid #999;
+}
+
+.preview img {
+  width: 70px;
+  height: 70px;
+}
+
+.rotate-right {
+  transform: rotate(90deg);
+}
+
+.rotate-left {
+  transform: rotate(-90deg);
+}
+
+
+
+
 .add-to-cart {
   position: absolute;
-  right: 30px;
-  width: 220px;
+  width: 310px;
   padding: 3px;
   font-size: 16px;
 }
